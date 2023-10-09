cd005f1338280bfd6e5d987f156f308047b9d872
LukaPolo <112101327+LukaPolo@users.noreply.github.com>
2023-10-09 01:34:57 -0300

    Movimiento del Jugador

--- Prototipo1_DVJ2/Assets/Scripts/CommandVoice.cs
+++ Prototipo1_DVJ2/Assets/Scripts/CommandVoice.cs
@@ -5,61 +5,62 @@ using UnityEngine;
 using UnityEngine.Windows.Speech;
 using System.Linq;
 
-public class CommandVoice : MonoBehaviour
-{
+public class CommandVoice : MonoBehaviour{
     // Start is called before the first frame update
     KeywordRecognizer keywordRecognizer; // creo mi reconocedor de comandode voz
-
     Dictionary<string, Action> actions = new Dictionary<string, Action>(); // creo las comando
+    private Vector3 direccion;
+    private float velocidad;
 
-    void Start()
-    {
+    void Start(){
         //creo las acciones segun el comandod e voz
+        actions.Add("up",MoverArriba);
+        actions.Add("down",MoverAbajo);
+        actions.Add("left",MoverIzquierda);
+        actions.Add("right",MoverDerecha);
+        actions.Add("stop",Parar);
+        actions.Add("arriba",MoverArriba);
         actions.Add("abajo",MoverAbajo);
+        actions.Add("izquierda",MoverIzquierda);
         actions.Add("derecha",MoverDerecha);
-        actions.Add("salto",Saltar);
-        //
-
-
+        actions.Add("para",Parar);
         //seteo el ronocedor de voz y lo inicio
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
-
-        //WindowsVoice.speak("Hola a los alumnos y alumnas de dise�o de videojuegos 2");
+        velocidad=1;
     }
 
-    private void Saltar()
-    {
-        Debug.Log("el personaje debe saltar");
-        //WindowsVoice.speak("he dicho saltar");
+    void Update(){
+        transform.Translate(direccion);
     }
 
-    private void MoverDerecha()
-    {
-        Debug.Log("el personaje debe moverse a la derecha");
-        //WindowsVoice.speak("he dicho derecha");
-
-    }
-
-    private void MoverAbajo()
-    {
-        Debug.Log("el personaje debe moverse hacia abajo");
-        //WindowsVoice.speak("he dicho aba");
-
-
-    }
-
-    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs command)
-    {
-       // Debug.Log(command.text);
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs command){
         actions[command.text].Invoke();
     }
 
-    /*private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K)) {
-            WindowsVoice.speak("tecla k");
-        }
-    }*/
+    private void MoverArriba(){
+        Debug.Log("ARRIBA");
+        direccion=new Vector3(0,0,velocidad*Time.deltaTime);
+    }
+
+    private void MoverAbajo(){
+        Debug.Log("ABAJO");
+        direccion=new Vector3(0,0,-(velocidad*Time.deltaTime));
+    }
+
+    private void MoverIzquierda(){
+        Debug.Log("IZQUIERDA");
+        direccion=new Vector3(-(velocidad*Time.deltaTime),0,0);
+    }
+
+    private void MoverDerecha(){
+        Debug.Log("DERECHA");
+        direccion=new Vector3(velocidad*Time.deltaTime,0,0);
+    }
+
+    private void Parar(){
+        Debug.Log("PARAR");
+        direccion=new Vector3(0,0,0);
+    }
 }
